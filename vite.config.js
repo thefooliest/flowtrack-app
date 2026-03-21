696f29565d273c3f477b29b983f0f0d52ac9e706
thefooliest <1xsdavidsx5@gmail.com>
2026-03-20 23:13:10 -0500

    added config for deployment with gh pages

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import basicSsl from '@vitejs/plugin-basic-ssl'
 
 export default defineConfig({
-  plugins: [react(), basicSsl()],
+  plugins: [react()],
+  base: '/flowtrack-app/',
   server: {
     host: true, // expone en tu red local
     port: 5173,
